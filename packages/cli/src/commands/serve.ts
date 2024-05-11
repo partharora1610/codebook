f272e8bcdcde2052e75fd826adc85bd5957cb908
partharora1610 <partharora16102001@gmail.com>
2024-05-12 02:14:16 +0530

    setup server for devlopment

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,10 +1,28 @@
 import { Command } from "commander";
+import { serve } from "local-api";
+import path from "path";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 export const serveCommand = new Command()
   .command("serve [filename]")
   .description("Open a file for editing")
   .option("-p, --port <number>", "port to run server on ", "4005")
-  .action((filename = "codebook.js", options) => {
-    console.log(options);
-    console.log("Getting ready to serve a file");
-  });
+  .action(
+    async (
+      filename = "codebook.js",
+      options: {
+        port: string;
+      }
+    ) => {
+      try {
+        const dir = path.join(process.cwd(), path.dirname(filename));
+        const file = path.basename(filename);
+        await serve(parseInt(options.port), file, dir, !isProduction);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log(error.message);
+        }
+      }
+    }
+  );
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,3 +1,36 @@
-export default () => {
-  console.log("server is listening in TS");
+import express, { Express } from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import path from "path";
+
+export const serve = (
+  port: number,
+  filename: string,
+  dir: string,
+  useProxy: boolean
+) => {
+  const app: Express = express();
+
+  if (useProxy) {
+    app.use(
+      createProxyMiddleware({
+        target: "http://127.0.0.1:5173",
+        ws: true,
+      })
+    );
+  } else {
+    const packagePath = require.resolve("local-client/dist/index.html");
+    // console.log(packagePath);
+    app.use(express.static(path.dirname(packagePath)));
+  }
+
+  return new Promise<void>((resolve, reject) => {
+    app
+      .listen(port, () => {
+        console.log(
+          `Opened ${filename}. Naivgate to the http://localhost:${port} to edit`
+        );
+        resolve();
+      })
+      .on("error", reject);
+  });
 };
